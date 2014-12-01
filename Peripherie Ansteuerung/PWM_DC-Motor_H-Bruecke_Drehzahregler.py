e8e942885117694a38cabc23806916e39e5928d7
David Huwyler <david.huwyler@gmail.com>
2014-12-01 20:24:40 +0100

    D-Anteil der Motorenregelung added

--- Peripherie Ansteuerung/PWM_DC-Motor_H-Bruecke_Drehzahregler.py
+++ Peripherie Ansteuerung/PWM_DC-Motor_H-Bruecke_Drehzahregler.py
@@ -46,30 +46,52 @@ GPIO.setup(18, GPIO.OUT)# set GPIO 25 as an output
   
 freq = 2000  
 p = GPIO.PWM(18, freq)    # create an object p for PWM
-duty = 40
+duty = 30
 p.start(duty)
-kp = 4
-ki = 0.01
-f_soll = input('Frequenz: ')
+kp = 10   #Verstärkung Proportional-Anteil
+ki = 0.5  #Verstärkung Integral-Anteil
+kd = 0.1  #Verstärkung Differential-Anteil
+
+e_sum = 0
+e_alt = 0
+
+delta_t = 0.1 #Abtastzeit (1/f)
+
+f_soll = input('Frequenz (min 5): ')
+if (f_soll < 5):
+  f_soll = 5
 
 try:
 
   while True:
-    #PI-Regler
+    #PID-Regler
     f_ist = getFrequenz()
-    e = f_soll-f_ist   #Regelabweichung
-    e_sum = e_sum + e  #integration des Fehlers
+    
+    e = f_soll-f_ist                #Regelabweichung
+    e_sum = e_sum + e               #integration des Fehlers
+    e_dif = (e - e_alt)/ delta_t    #differentiation des Fehlers
+    e_alt = e
+    if (e_sum > 1000000):
+      e_sum = 1000000
+    if (e_sum < -1000000):
+      e_sum = -1000000
 
     y_p = kp*e
-    y_i = ky*e_sum
+    y_i = ki*e_sum
+    y_d = kd*e_dif
 
-    y= y_p + y_i
+    y= y_p + y_i + y_d
     
     duty = duty + y
-    
+    if (duty > 100):
+      duty = 100
+    if (duty < 0):
+      duty = 0
+      
+    print "Frequenz :" + str(freq) + "Duty :" +str(duty)
     
     p.ChangeDutyCycle(duty)
-    time.sleep(0.1)
+    time.sleep(delta_t)
 
 except KeyboardInterrupt:
   # User pressed CTRL-C
