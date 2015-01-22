2fb678f3c3e55d97063199f5e48b1e5977a20e8f
David Huwyler <david.huwyler@gmail.com>
2015-01-22 16:03:55 +0100

    UART Communitatoin Script added

---
+++ Peripherie Ansteuerung/UART.py
@@ -0,0 +1,28 @@
+# Strellt eine UART Verbindung her und
+# Sendet die den Gewuenschten String
+# Author : David Huwyler
+# Date   : 28.11.2014
+# -----------------------
+# -----------------------
+# Import required Python libraries
+# -----------------------
+import serial
+import sys
+  
+# -----------------------
+# Main Script
+# -----------------------
+UART = serial.Serial("/dev/ttyAMA0", 38400)
+
+try:
+
+  while True:
+    string = input('String: ')
+    UART.open()
+    UART.write(string)
+    UART.close()
+	
+except KeyboardInterrupt:
+  # User pressed CTRL-C
+  # Reset GPIO settings
+  GPIO.cleanup()
