c3dcde03b5983c495315b1d31744d1cdfb6a84c6
SeverinEggermann <severin.eggermann@stud.hslu.ch>
2015-02-19 11:17:38 +0100

    Ordnerstruktur erstellt

---
+++ src/T37_PREN/PythonInterop/PythonHandler.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package T37_PREN.PythonInterop;
+
+/**
+ *
+ * @author Severin
+ */
+public class PythonHandler {
+    
+}
