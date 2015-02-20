8cf7bdff1fa2a7f788a303db943b2a56c984ea8a
SeverinEggermann <severin.eggermann@stud.hslu.ch>
2015-02-20 10:54:13 +0100

    Python Handler added

--- src/T37_PREN/PythonInterop/PythonHandler.java
+++ src/T37_PREN/PythonInterop/PythonHandler.java
@@ -3,13 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package T37_PREN.PythonInterop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author Severin
  */
-public class PythonHandler {
-    
+public final class PythonHandler {
+
+    private Process _pythonProcess;
+
+    public PythonHandler() {
+    }
+
+    public PythonHandler(String scriptName, String scriptArguments) {
+        startPythonScript(scriptName, scriptArguments);
+    }
+
+    public void startPythonScript(String scriptName, String scriptArguments) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", scriptName, scriptArguments);
+            _pythonProcess = processBuilder.start();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getPythonOutput() throws Exception {
+        String pythonOutput = "";
+        if (!isScriptInitialized()) {
+            throw new Exception("Script must be Initialized!");
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(_pythonProcess.getInputStream()));
+        //int ret = new Integer(in.readLine()).intValue();
+        pythonOutput = in.readLine();
+
+        return pythonOutput;
+    }
+
+    public void stopPythonProcess() throws InterruptedException {
+        if (isScriptInitialized()) {
+            _pythonProcess.destroy();
+            _pythonProcess.waitFor();
+        }
+    }
+
+    private boolean isScriptInitialized() {
+        return _pythonProcess != null;
+    }
+
 }
