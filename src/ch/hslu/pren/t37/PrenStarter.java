8e01011ec9e8c768aa70f4c02162b1425a32605c
Alex <Alex@WPN01>
2015-04-16 10:14:33 +0200

    Error Handling

--- src/ch/hslu/pren/t37/PrenStarter.java
+++ src/ch/hslu/pren/t37/PrenStarter.java
@@ -5,14 +5,10 @@
  */
 package ch.hslu.pren.t37;
 
-import ch.hslu.pren.t37.camera.BildAuswertungKorb;
-import ch.hslu.pren.t37.camera.BildVonWebcamAufnehmen;
-import ch.hslu.pren.t37.logic.DCEngineHandler;
 import ch.hslu.pren.t37.logic.Logic;
-import ch.hslu.pren.t37.logic.StepperMagazine;
-import ch.hslu.pren.t37.logic.StepperTurret;
-import ch.hslu.pren.t37.logic.UltrasonicHandler;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrenStarter {
 
@@ -26,75 +22,9 @@ public class PrenStarter {
         try {
             Logic controllerLogic = new Logic();
             controllerLogic.initialRun();
-        } catch (Exception ex) {
+        } catch (IOException | InterruptedException ex) {
             // ToDO: start failover logic
-        }
-        //cam();
-        //ultraschall();
-        //stepperDrehturm();
-        //stepperFeedingBalls();
-        //dcEngineStart();
-        //dcEngineStop();
+            Logger.getLogger(PrenStarter.class.getName()).log(Level.SEVERE, null, ex);
+        }       
     }
-
-    public static void cam() throws InterruptedException {
-        //Foto aufnehmen
-        BildVonWebcamAufnehmen myPictureFromWebcam = new BildVonWebcamAufnehmen("../PeripherieAnsteuerung/Ready for Pi/Camera_PI_FINAL.py", new ArrayList<String>());
-        myPictureFromWebcam.runPythonScript();
-        myPictureFromWebcam.stopPythonProcess();
-
-        //Foto auswerten
-        BildAuswertungKorb myBildauswertung = new BildAuswertungKorb();
-        myBildauswertung.bildAuswerten();
-
-        //Berechnen der Drehung des Turmes
-        //erfolgt in der BildAuswertungsKlasse 
-    }
-
-    public static void ultraschall() {
-        //UltrasonicHandler ultrasonicHandler = new UltrasonicHandler("C:\\Users\\Severin\\Documents\\NetBeansProjects\\PythonPREN\\PeripherieAnsteuerung\\test1.py", new ArrayList<String>());
-        UltrasonicHandler ultrasonicHandler = new UltrasonicHandler("../PeripherieAnsteuerung/Ready for Pi/ultrasonic_PI_FINAL.py", new ArrayList<String>());
-        ultrasonicHandler.runPythonScript();
-        ultrasonicHandler.evaluateScriptOutput();
-        ultrasonicHandler.stopPythonProcess();
-    }
-
-    public static void stepperDrehturm() {
-        ArrayList<String> argsP = new ArrayList<>();
-        argsP.add("10"); //Anzahl Schritte (48 ist eine Umdrehung)
-        argsP.add("0"); //Nur 0 oder 1
-        //StepperTurret stepperTurret = new StepperTurret("C:\\Users\\Severin\\Documents\\NetBeansProjects\\PythonPREN\\PeripherieAnsteuerung\\test2.py", argsP);
-        StepperTurret stepperTurret = new StepperTurret("../PeripherieAnsteuerung/Ready for Pi/Stepper_Drehturm_PI_FINAL.py", argsP);
-        stepperTurret.runPythonScript();
-        stepperTurret.stopPythonProcess();
-    }
-
-    public static void stepperFeedingBalls() {
-        ArrayList<String> argsP = new ArrayList<>();
-        argsP.add("1"); //wird * 100 gerechnet
-        argsP.add("0"); //Nur 0 oder 1
-        //StepperFeedingBalls stepperFeedingBalls = new StepperMagazine("C:\\Users\\Severin\\Documents\\NetBeansProjects\\PythonPREN\\PeripherieAnsteuerung\\test2.py", argsP);
-        StepperMagazine stepperFeedingBalls = new StepperMagazine("../PeripherieAnsteuerung/Ready for Pi/Stepper_Zufuerung_PI_FINAL.py", argsP);
-        stepperFeedingBalls.runPythonScript();
-        stepperFeedingBalls.stopPythonProcess();
-    }
-
-    public static void dcEngineStart() throws InterruptedException {
-        ArrayList<String> argsP = new ArrayList<>();
-        argsP.add("010");
-        //DCEngineHandler dcEngineHandler = new DCEngineHandler("C:\\Users\\Severin\\Documents\\NetBeansProjects\\PythonPREN\\PeripherieAnsteuerung\\test2.py", argsP);
-        DCEngineHandler dcEngineHandler = new DCEngineHandler("../PeripherieAnsteuerung/Ready for Pi/UART_PI_FINAL.py", argsP);
-        dcEngineHandler.runPythonScript();
-        dcEngineHandler.stopPythonProcess();
-    }
-
-    public static void dcEngineStop() {
-        ArrayList<String> argsP = new ArrayList<>();
-        argsP.add("000");
-        //DCEngineHandler dcEngineHandler = new DCEngineHandler("C:\\Users\\Severin\\Documents\\NetBeansProjects\\PythonPREN\\PeripherieAnsteuerung\\test2.py", argsP);
-        DCEngineHandler dcEngineHandler = new DCEngineHandler("../PeripherieAnsteuerung/Ready for Pi/UART_PI_FINAL.py", argsP);
-        dcEngineHandler.runPythonScript();
-        dcEngineHandler.stopPythonProcess();
-    }
-
 }
